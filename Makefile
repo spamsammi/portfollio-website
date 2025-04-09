32383c03a0bfc76810d8890f2b842eb93c9cfc89
Luke Henry <luke.d.henry1@gmail.com>
2025-04-09 07:36:10 -0500

    added makefile arguments, ensure local build works

--- Makefile
+++ Makefile
@@ -2,3 +2,14 @@
 prerequisites:
 	@eval command -v npm > /dev/null 2>&1 || (echo "npm is not installed" && exit 1)
 	
+run:
+	npm run develop
+
+build:
+	npm run build
+
+serve:
+	npm run serve
+
+clean:
+	npm run clean
--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,14 +2,14 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `portfollio-website`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `dani-henry`,
+    siteUrl: `https://www.danihenry.com`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
