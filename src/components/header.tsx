5d08ae8d6fd5bb0dc803d82a655ffe7dfca3f21b
Luke Henry <luke.d.henry1@gmail.com>
2025-04-13 21:20:18 -0500

    inital tailwinds navbar attempt

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,24 +11,48 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ siteTitle, menuLinks }) => {
     return (
-        <header>
-            <h1>
-                <Link to="/">
-                    {siteTitle}
-                </Link>
-            </h1>
-            <div>
-                <nav>
-                    <ul>
-                    {menuLinks.map(link => (
-                        <li key={link.name}>
-                            <Link to={link.link}>
-                                {link.name}
-                            </Link>
-                        </li>
-                    ))}
-                    </ul>
-                </nav>
+        <header className="bg-[#FFDAB9]">
+            <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+                <div className="flex h-16 items-center justify-between">
+                    <h1>
+                        <Link to="/" className="block text-[#DC143C] text-xl font-bold">
+                            {siteTitle}
+                        </Link>
+                    </h1>
+                    <div className="md:flex md:items-center md:gap-12">
+                        <nav aria-label="Global" className="hidden md:block">
+                            <ul className="flex items-center gap-6 text-sm">
+                                {menuLinks.map(link => (
+                                    <li key={link.name}>
+                                        <Link 
+                                            to={link.link}
+                                            className="text-[#DC143C] transition hover:text-[#DC143C]/75"
+                                        >
+                                            {link.name}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </nav>
+
+                        <div className="block md:hidden">
+                            <button
+                                className="rounded-sm bg-[#FFB6A3] p-2 text-[#DC143C] transition hover:text-[#DC143C]/75"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-5 w-5"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                >
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </header>
     )
