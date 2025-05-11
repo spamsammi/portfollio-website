44f1202d287fa2f27a4db5f7b434a4be9b2465b8
Luke Henry <luke.d.henry1@gmail.com>
2025-05-10 21:19:40 -0500

    more runner work #2

--- bin/setup-runner.py
+++ bin/setup-runner.py
@@ -16,7 +16,7 @@ dependencies = [
 def check_dependencies():
     for dependency in dependencies:
         try:
-            subprocess.run([dependency], check=True)
+            subprocess.run(dependency.split(' '), check=True)
         except subprocess.CalledProcessError:
             command = ' '.join(dependency.split(' ')[:-1])
             print(f"Error: {command} is not installed")
@@ -31,45 +31,45 @@ def is_valid_git_repo(repo_url):
         return False
 
 def main():
-  # Make sure this runner has the necessary dependencies
-  check_dependencies()
+    # Make sure this runner has the necessary dependencies
+    check_dependencies()
 
-  # Must provide the https url to the repo that you are going to clone from. This may seem redundant, but it's
-  # to prevent any inccorrect repoisitories getting cloned if this repository is forked.
-  repo_url = None
-  runner_working_dir = os.getcwd()
-  save_dir = runner_working_dir
-  os.chdir(runner_working_dir)
+    # Must provide the https url to the repo that you are going to clone from. This may seem redundant, but it's
+    # to prevent any inccorrect repoisitories getting cloned if this repository is forked.
+    repo_url = None
+    working_dir = os.getcwd()
+    save_dir = working_dir
+    os.chdir(working_dir)
 
-  # Script is meant to be run via curl, so we need to manually parse the arguments to avoid needing dependencies
-  for arg in sys.argv[1:]:
-      if arg.startswith("--repo-url="):
-          repo_url = arg.split("=", 1)[1]
-      if arg.startswith("--runner-working-dir="):
-          runner_working_dir = arg.split("=", 1)[1]
+    # Script is meant to be run via curl, so we need to manually parse the arguments to avoid needing dependencies
+    for arg in sys.argv[1:]:
+        if arg.startswith("--repo-url="):
+            repo_url = arg.split("=", 1)[1]
+        if arg.startswith("--runner-working-dir="):
+            working_dir = arg.split("=", 1)[1]
 
-  # Check to make sure that the repo url is provided and valid
-  if repo_url is None:
-      print("Error: --repo-url is required")
-      sys.exit(1)
-  if not is_valid_git_repo(repo_url):
-      print("Error: --repo-url is not a valid git repository")
-      sys.exit(1)
+    # Check to make sure that the repo url is provided and valid
+    if repo_url is None:
+        print("Error: --repo-url is required")
+        sys.exit(1)
+    if not is_valid_git_repo(repo_url):
+        print("Error: --repo-url is not a valid git repository")
+        sys.exit(1)
 
-  print(f"Setting up runner with repo: {repo_url} in working dir: {runner_working_dir}")
-  os.makedirs(runner_working_dir, exist_ok=True)
-  # Clone the repo in "dev" and "test" directories
-  try:
-      subprocess.run(["git", "clone", repo_url, "dev"])
-      subprocess.run(["git", "clone", repo_url, "test"])
-  except subprocess.CalledProcessError as e:
-      print(f"Error: Failed to clone a repository: {e}")
-      sys.exit(1)
+    print(f"Setting up runner with repo: {repo_url} in working dir: {working_dir}")
+    os.makedirs(working_dir, exist_ok=True)
+    # Clone the repo in "dev" and "test" directories
+    try:
+        subprocess.run(["git", "clone", repo_url, "dev"])
+        subprocess.run(["git", "clone", repo_url, "test"])
+    except subprocess.CalledProcessError as e:
+        print(f"Error: Failed to clone a repository: {e}")
+        sys.exit(1)
 
-  # Create github actions workflow files
-  os.makedirs(".github/workflows", exist_ok=True)
-  with open(".github/workflows/deploy.yml", "w") as f:
-      f.write("""name: Deploy Application
+    # Create github actions workflow files
+    os.makedirs(".github/workflows", exist_ok=True)
+    with open(".github/workflows/deploy.yml", "w") as f:
+        f.write(f"""name: Deploy Application
 
 on:
   push:
@@ -79,6 +79,9 @@ on:
 jobs:
   deploy:
     runs-on: self-hosted
+	defaults:
+      run:
+        working-directory: {working_dir}
 
     steps:
       - name: Checkout code
@@ -104,9 +107,9 @@ jobs:
       - name: Clean up old Docker images
         run: docker image prune -f""")
 
-  # Create .env file
-  with open(".env", "w") as f:
-      f.write("""# Development environment settings
+    # Create .env file
+    with open(".env", "w") as f:
+        f.write("""# Development environment settings
 DEV_DIR=./dev
 DEV_PORT=8000
 
@@ -115,16 +118,15 @@ TEST_DIR=./test
 TEST_PORT=8001
 """)
 
-
-  # Setup .gitignore file
-  with open(".gitignore", "w") as f:
-      f.write("""dev/
+    # Setup .gitignore file
+    with open(".gitignore", "w") as f:
+        f.write("""dev/
 test/
 .env
 """)
         
-  # Swap back to the original working directory
-  os.chdir(save_dir)
+    # Swap back to the original working directory
+    os.chdir(save_dir)
 
 if __name__ == "__main__":
     main()
